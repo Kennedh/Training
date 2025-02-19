7772d273d8140de80ecc4b570f7388fb264e30be
kennedh <ken.cmarcellino@gmail.com>
2025-02-19 19:25:59 -0300

    feat: Adicionado funções de adicionar / remover notas e calcular média

--- gerenciador_de_notas.py
+++ gerenciador_de_notas.py
@@ -9,3 +9,41 @@
 
 💡 Extras: Armazene as notas em um dicionário com nomes dos alunos e permita ver a média individual.
 """
+notas = []
+
+def adicionar_nota():
+    print("Digite uma nota: ")
+    notas.append(input())
+
+def remover_nota():
+    print("Digite a nota que deseja remover: ")
+    print(f"Notas digitas: {notas}")
+    notas.remove(input())
+
+def calcular_media():
+    total = 0
+    for n in notas:
+        total += float(n)
+    print(total / len(notas))
+
+print("Gerenciamento de notas\n")
+print("----------------------\n")
+
+opcao = 0
+
+while True:
+    print("1 - Adicionar nota")
+    print("2 - Remover nota")
+    print("3 - Calcular média")
+    print("4 - Sair")
+    opcao = int(input())
+    if opcao == 1:
+        adicionar_nota()
+    if opcao == 2:
+        remover_nota()
+    if opcao == 3:
+        calcular_media()
+    if opcao == 4:
+        break
+    if opcao > 4:
+        print("Opção incorreta!")
