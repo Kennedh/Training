3a93080c7c0f2be12f09accf6cc6424981dcdcc7
kennedh <ken.cmarcellino@gmail.com>
2025-12-23 18:51:50 -0300

    Update Finalizado logica

--- Sistema de Triagem de Hospital.py
+++ Sistema de Triagem de Hospital.py
@@ -21,14 +21,35 @@ José (Gravidade 2)
 """
 
 fila_hospital = []
+contador_chegada = 0
+
 
 def adicionar_paciente(nome, gravidade):
-    pass
+    global contador_chegada
+    contador_chegada += 1
+    fila_hospital.append({
+        "nome": nome,
+        "gravidade": gravidade,
+        "ordem_chegada": contador_chegada
+    })
+
 
 def atender_proximo():
     if not fila_hospital:
         return "Nenhum paciente na fila."
-    pass
+
+    paciente_prioritario = fila_hospital[0]
+
+    for paciente in fila_hospital[1:]:
+        if paciente["gravidade"] > paciente_prioritario["gravidade"]:
+            paciente_prioritario = paciente
+        elif (paciente["gravidade"] == paciente_prioritario["gravidade"] and
+              paciente["ordem_chegada"] < paciente_prioritario["ordem_chegada"]):
+            paciente_prioritario = paciente
+
+    fila_hospital.remove(paciente_prioritario)
+
+    return f"{paciente_prioritario['nome']} (Gravidade {paciente_prioritario['gravidade']})"
 
 # Teste
 adicionar_paciente("Tiago", 2)
@@ -36,5 +57,7 @@ adicionar_paciente("Ana", 4)
 adicionar_paciente("Beto", 4)
 adicionar_paciente("Carlos", 1)
 
-print(atender_proximo()) # Deve ser Ana
-print(atender_proximo()) # Deve ser Beto (mesma prioridade, mas chegou depois)
+print(atender_proximo())
+print(atender_proximo())
+
+print(fila_hospital)
