febf3f170f4a50e81b972d8b186c26a163335d96
kennedh <ken.cmarcellino@gmail.com>
2026-05-18 14:56:41 -0300

    create banco de dados simples

---
+++ crud_ctk/banco.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+class Banco:
+    def __init__(self):
+        self.conexao = sqlite3.connect("aplicativo.db")
+        self.cursor = self.conexao.cursor()
+        self.criar_tabelas()
+
+    def criar_tabelas(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT UNIQUE,
+            email text,
+            usuario TEXT UNIQUE,
+            senha text
+        )""")
+
+    def inserir_usuario(self, nome, email, usuario, senha):
+        try:
+            self.cursor.execute("INSERT INTO usuarios "
+                                    "(nome, email, usuario, senha) VALUES (?, ?, ?, ?)",
+                           (nome, email, usuario, senha))
+            self.conexao.commit()
+            return "Usuário Cadastrado"
+        except sqlite3.IntegrityError:
+            return "Usuário não cadastrado"
